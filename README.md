c0d042ed2d2e857cdbfea1e88bbf2cc1dec6e80e
April Shen <april.shen@babylonhealth.com>
2019-06-26 19:01:41 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# quags-and-quasits
-Generates nonsense
+# Quags and Quasits
+
+Generates nonsense.
+
+Dungeons are from [WordNet](https://wordnet.princeton.edu/).
+Dragons are from [Kobold Fight Club](https://github.com/Asmor/5e-monsters).
---
+++ qnq.py
@@ -0,0 +1,88 @@
+from collections import defaultdict
+import csv
+import json
+import numpy as np
+import os
+
+from nltk.corpus import wordnet as wn
+from textblob import TextBlob
+# import epitran
+
+# epi = epitran.Epitran('eng-Latn')
+DATA_DIR = 'data'
+
+
+def munge(s):
+    blob = TextBlob(s)
+    return ' '.join(blob.words[:-1] + [blob.words[-1].pluralize()]).title()
+
+
+def prep(save=False):
+    location = [
+        wn.synset('location.n.01'),
+        wn.synset('building.n.01'),
+        wn.synset('geological_formation.n.01'),
+        wn.synset('land.n.02'),
+        wn.synset('vegetation.n.01')
+    ]
+    hypo = lambda s: s.hyponyms()
+    locs = list({
+        z.replace('_', ' ')
+        for x in location
+        for y in x.closure(hypo)
+        for z in y.lemma_names()
+    })
+
+    # with open(os.path.join(DATA_DIR, 'monsters.json'), 'r') as f:
+    #     data = json.load(f)
+    # names = [x['name'] for x in data]
+    with open(os.path.join(DATA_DIR, 'kfc-monsters.csv'), 'r') as f:
+        reader = csv.reader(f)
+        names = [row[2] for row in reader][1:]
+
+    locs = [munge(x) for x in locs]
+    names = [munge(x) for x in names]
+
+    # locs_ipa = [epi.transliterate(w) for w in locs]
+    # names_ipa = [epi.transliterate(w) for w in names]
+
+    dungeons = defaultdict(list)
+    for i in range(len(locs)):
+        dungeons[locs[i][0]].append(locs[i])
+
+    dragons = defaultdict(list)
+    for i in range(len(names)):
+        dragons[names[i][0]].append(names[i])
+
+    if save:
+        with open(os.path.join(DATA_DIR, 'dungeons.json'), 'w+') as f:
+            json.dump(dungeons, f)
+        with open(os.path.join(DATA_DIR, 'dragons.json'), 'w+') as f:
+            json.dump(dragons, f)
+
+    return dungeons, dragons
+
+
+def sample(n=1, load=True, dungeons=None, dragons=None):
+    if load:
+        with open(os.path.join(DATA_DIR, 'dungeons.json'), 'r') as f:
+            dungeons = json.load(f)
+        with open(os.path.join(DATA_DIR, 'dragons.json'), 'r') as f:
+            dragons = json.load(f)
+
+    results = []
+    for _ in range(n):
+        options = list(set(dungeons.keys()).intersection(dragons.keys()))
+        probs = np.array([len(dungeons[l]) + len(dragons[l]) for l in options], dtype='float64')
+        probs /= probs.sum()
+        letter = np.random.choice(options, p=probs)
+        dungeon = np.random.choice(dungeons[letter])
+        dragon = np.random.choice(dragons[letter])
+        results.append(f'{dungeon} and {dragon}')
+    return results
+
+
+if __name__ == '__main__':
+    # dungeons, dragons = prep(save=True)
+    for x in sample(n=10):
+        print(x)
